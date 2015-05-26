761f3ed32073c4e6b83b2d41049807cc1107c22d
RobertAKARobin <robertgfthomas@gmail.com>
2015-05-26 16:14:29 -0400

    Spaghetti, but ie8 compatible

--- scripts.js
+++ scripts.js
@@ -1,79 +1,55 @@
-function forEach(collection, callback){
-  for(var x = collection.length - 1; x >= 0; x--){
-    callback.call(this, collection[x]);
-  }
-}
-
 var Draggable = function(el){
+  var self = this;
   this.el = el;
   this.el.style.left = "0px";
   this.el.style.top = "0px";
   this.origin = {};
+  this.events = {};
 
-  this.handle = {
-    drag: this.drag.bind(this),
-    move: this.move.bind(this)
+  function listener(el, events, callback, start){
+    if(typeof events === "string") events = new Array(events);
+    for(var x = events.length - 1; x >= 0; x--){
+      if(start){
+        if(el.attachEvent){
+          el.attachEvent("on" + events[x], callback);
+        }else{
+          el.addEventListener(events[x], callback);
+        }
+      }else{
+        if(el.detachEvent){
+          el.detachEvent("on" + events[x], callback);
+        }else{
+          el.removeEventListener(events[x], callback);
+        }
+      }
+    }
   }
 
-  this.events = {
-    start: new Listener(this.el, ["mousedown", "touchstart"], this.handle.drag),
-    move: {},
-    end: {}
-  }
-}
-Draggable.prototype = {
-  drag: function(evt){
-    this.origin.left = parseInt(this.el.style.left) - evt.clientX;
-    this.origin.top = parseInt(this.el.style.top) - evt.clientY;
-    this.events.move = new Listener(window, ["mousemove", "touchmove"], this.handle.move);
-    this.events.end = new Listener(window, ["mouseup", "touchend"], this.drop.bind(this));
-  },
-  move: function(evt){
-    this.el.style.left = this.origin.left + evt.clientX + "px";
-    this.el.style.top = this.origin.top + evt.clientY + "px";
-  },
-  drop: function(){
-    this.events.move.stopListening();
-  }
-}
-
-var Listener = function(el, triggers, callback){
-  this.el = el;
-  this.triggers = [];
-  this.callback = function(evt){
-    evt.preventDefault();
-    if(evt instanceof TouchEvent){
+  function drag(evt){
+    if(!evt.clientX){
       evt = evt.touches[0];
     }
-    callback(evt);
+    self.origin.left = parseInt(self.el.style.left) - evt.clientX;
+    self.origin.top = parseInt(self.el.style.top) - evt.clientY;
+    listener(document, ["mousemove", "touchmove"], move, true);
+    listener(document, ["mouseup", "touchend"], drop, true);
   }
 
-  if(typeof triggers === "string"){
-    this.triggers.push(triggers);
-  }else{
-    this.triggers = triggers;
-  }
-  forEach.call(this, this.triggers, function(trigger){
-    this.listenFor(trigger, this.callback);
-  });
-}
-Listener.prototype = {
-  listenFor: function(trigger){
-    if(this.el.attachEvent){
-      this.el.attachEvent(trigger, this.callback); 
-    }else{
-      this.el.addEventListener(trigger, this.callback);
+  function move(evt){
+    if(!evt.clientX){
+      evt = evt.touches[0];
     }
-  },
-  stopListening: function(){
-    forEach.call(this, this.triggers, function(trigger){
-      if(this.el.detachEvent){
-        this.el.detachEvent(trigger, this.callback);
-      }else{
-        this.el.removeEventListener(trigger, this.callback);
-      }
-    });
+    self.el.style.left = self.origin.left + evt.clientX + "px";
+    self.el.style.top = self.origin.top + evt.clientY + "px";
   }
+
+  function drop(){
+    listener(document, ["mousemove", "touchmove"], move, false);
+    listener(document, ["mouseup", "touchend"], drop, false);
+  }
+
+  listener(this.el, ["mousedown", "touchstart"], drag, true);
+
 }
 
 var Tags = function(){
